fe4957d87fa727557cd90aa88e2518e035f20491
abhi-arya1 <abhia9167@gmail.com>
2023-12-18 21:01:08 -0800

    update routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home/>} />
-              <Route path="/about" element={<About />} />
+              <Route path="/team-cartographer-site/" element={<Home/>} />
+              <Route path="/team-cartographer-site/about" element={<About />} />
 
               {/* FIXME: implement this <Route path="/contact" element={<ContactForm />} /> */}
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,14 +7,14 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <img src={logo} alt="Team Cartographer Logo" className="navbar-logo" /> 
-      <Link to="/" className="site-title">
+      <Link to="/team-cartographer-site" className="site-title">
         Team Cartographer
       </Link>
 
       <Dropdown title="Links">
         <a href="https://github.com/Team-Cartographer">GitHub</a>
         <a href="https://www.linkedin.com/company/team-cartographer/">LinkedIn</a>
-        <Link className="AboutUs" to="/about">About Us</Link>
+        <Link className="AboutUs" to="/team-cartographer-site/about">About Us</Link>
         <a href="mailto:teamcartographer@gmail.com">Contact Us</a>
       </Dropdown>
     </nav>
